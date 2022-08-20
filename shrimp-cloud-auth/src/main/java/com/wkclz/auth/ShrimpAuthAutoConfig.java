40b19fda08256c780f10452f0295305cc21a49aa
wangkc <wkclz@qq.com>
2022-08-20 16:23:56 +0800

    自助扫描 Servlet 相关

--- shrimp-cloud-auth/src/main/java/com/wkclz/auth/ShrimpAuthAutoConfig.java
+++ shrimp-cloud-auth/src/main/java/com/wkclz/auth/ShrimpAuthAutoConfig.java
@@ -1,9 +1,16 @@
 package com.wkclz.auth;
 
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 在SpringBootApplication上使用@ServletComponentScan注解后，
+ * Servlet、Filter、Listener可以直接通过@WebServlet、@WebFilter、@WebListener注解自动注册，
+ * 无需其他代码。
+ */
 @Configuration
 @ComponentScan(basePackages = {"com.wkclz.auth"})
+@ServletComponentScan(basePackages = {"com.wkclz.auth"})
 public class ShrimpAuthAutoConfig {
 }
