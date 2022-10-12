d21f6867fb4d9e9bc9881c3fb0f6a204dc121ac3
wangkc <wkclz@qq.com>
2022-10-12 23:24:05 +0800

    mqtt 模块更换模块名和路径

--- shrimp-cloud-mqtt/src/main/java/com/wkclz/mqtt/MqttAutoConfigure.java
+++ shrimp-cloud-mqtt/src/main/java/com/wkclz/mqtt/MqttAutoConfigure.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"com.wkclz.mqtt"})
-public class StarterAutoConfigure {
+public class MqttAutoConfigure {
 }
 
 
