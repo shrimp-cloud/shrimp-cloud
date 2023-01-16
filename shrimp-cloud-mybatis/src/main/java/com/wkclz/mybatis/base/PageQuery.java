91f5e54ba0ee270c17dec502788d3f1816327aa3
wangkc <wkclz@qq.com>
2023-01-16 22:22:53 +0800

    分页方法，显示的清理分页线程信息

--- shrimp-cloud-mybatis/src/main/java/com/wkclz/mybatis/base/PageQuery.java
+++ shrimp-cloud-mybatis/src/main/java/com/wkclz/mybatis/base/PageQuery.java
@@ -7,44 +7,23 @@ import com.wkclz.common.entity.BaseEntity;
 import java.util.List;
 import java.util.function.Function;
 
-public class PageQuery<T extends BaseEntity> {
+public class PageQuery {
 
-    private T param;
+    public static <T extends BaseEntity> PageData page(T param, Function<T, List> function) {
+        try {
+            param.init();
+            int current = param.getCurrent().intValue();
+            int size = param.getSize().intValue();
+            PageHelper.startPage(current, size);
 
-    public PageQuery(T param){
-        param.init();
-        this.param = param;
-        PageHelper.startPage(param.getCurrent().intValue(), param.getSize().intValue());
-    }
-    public PageData page(List list){
-        Page listPage = (Page) list;
-        PageData<T> pageData = new PageData<>(param);
-        pageData.setTotal(listPage.getTotal());
-        pageData.setRows(listPage.getResult());
-        return pageData;
-    }
-
-    public static <T extends BaseEntity> PageQuery param(T param) {
-        return new PageQuery<>(param);
-    }
-
-    public PageData page(Function<T, List> function){
-        Page listPage = (Page)function.apply(param);
-        PageData<T> pageData = new PageData<>(param);
-        pageData.setTotal(listPage.getTotal());
-        pageData.setRows(listPage.getResult());
-        return pageData;
-    }
-
-    public static <T extends BaseEntity> PageData page(T param, Function<T, List> function){
-        param.init();
-        PageHelper.startPage(param.getCurrent().intValue(), param.getSize().intValue());
-
-        Page listPage = (Page)function.apply(param);
-        PageData<T> pageData = new PageData<>(param);
-        pageData.setTotal(listPage.getTotal());
-        pageData.setRows(listPage.getResult());
-        return pageData;
+            Page listPage = (Page)function.apply(param);
+            PageData<T> pageData = new PageData<>(param);
+            pageData.setTotal(listPage.getTotal());
+            pageData.setRows(listPage.getResult());
+            return pageData;
+        } finally {
+            PageHelper.clearPage();
+        }
     }
 
 }
