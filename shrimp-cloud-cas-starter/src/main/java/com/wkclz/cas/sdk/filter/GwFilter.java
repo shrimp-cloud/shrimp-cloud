98d58b09b249c2c57bca402c171b356619e5276b
wangkc <wkclz@qq.com>
2023-03-22 00:02:21 +0800

    权限过滤器统一拦截异常

--- shrimp-cloud-cas-starter/src/main/java/com/wkclz/cas/sdk/filter/GwFilter.java
+++ shrimp-cloud-cas-starter/src/main/java/com/wkclz/cas/sdk/filter/GwFilter.java
@@ -85,6 +85,41 @@ public class GwFilter extends OncePerRequestFilter {
         try {
             userCode = authHelper.getUserCode();
             logger.info("request: {}, userCode: {}, UA: {}", uri, userCode, ua);
+
+
+            /* 因为所有应用都走同一个入口，无法获取模块应用编码。无法使用区分授权方式
+            AppInfo appInfo = appInfoCache.get(authHelper.getAppCode());
+            String authType = appInfo.getApp().getAuthType();
+            if ("TOKEN".equals(authType)) {
+                chain.doFilter(request, response);
+                return;
+            }
+            if ("TOKEN_API".equals(authType) || "TOKEN_API_BUTTON".equals(authType) ) {
+                String method = request.getMethod();
+                boolean b = dUserApiCache.get(method, uri);
+                if (!b) {
+                    Result msg = Result.error("没有接口权限: " + uri);
+                    msg.setCode(HttpStatus.FORBIDDEN.value());
+                    ResponseHelper.responseError(response, msg);
+                    return;
+                }
+                chain.doFilter(request, response);
+                return;
+            }
+            */
+
+
+            // 全部都启用接口级鉴权 begin
+            String method = request.getMethod();
+            boolean b = dUserApiCache.get(method, uri);
+            if (!b) {
+                logger.info("request: {}, no permission: user: {} UA: {}", uri, userCode, ua);
+                Result msg = Result.error("没有接口权限: " + uri);
+                msg.setCode(HttpStatus.FORBIDDEN.value());
+                ResponseHelper.responseError(response, msg);
+                return;
+            }
+            // 全部都启用接口级鉴权 end
         } catch (Exception e) {
             String msg = e.getMessage();
             Result error = Result.error(msg);
@@ -96,40 +131,6 @@ public class GwFilter extends OncePerRequestFilter {
             ResponseHelper.responseError(response, error);
             return;
         }
-
-        /* 因为所有应用都走同一个入口，无法获取模块应用编码。无法使用区分授权方式
-        AppInfo appInfo = appInfoCache.get(authHelper.getAppCode());
-        String authType = appInfo.getApp().getAuthType();
-        if ("TOKEN".equals(authType)) {
-            chain.doFilter(request, response);
-            return;
-        }
-        if ("TOKEN_API".equals(authType) || "TOKEN_API_BUTTON".equals(authType) ) {
-            String method = request.getMethod();
-            boolean b = dUserApiCache.get(method, uri);
-            if (!b) {
-                Result msg = Result.error("没有接口权限: " + uri);
-                msg.setCode(HttpStatus.FORBIDDEN.value());
-                ResponseHelper.responseError(response, msg);
-                return;
-            }
-            chain.doFilter(request, response);
-            return;
-        }
-        */
-
-        // 全部都启用接口级鉴权 begin
-        String method = request.getMethod();
-        boolean b = dUserApiCache.get(method, uri);
-        if (!b) {
-            logger.info("request: {}, no permission: user: {} UA: {}", uri, userCode, ua);
-            Result msg = Result.error("没有接口权限: " + uri);
-            msg.setCode(HttpStatus.FORBIDDEN.value());
-            ResponseHelper.responseError(response, msg);
-            return;
-        }
-        // 全部都启用接口级鉴权 end
-
         chain.doFilter(request, response);
 
         /*
