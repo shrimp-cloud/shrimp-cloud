4ddbbbf59ff77e795e0b79506087054d3b1df3d9
wangkc <wkclz@qq.com>
2023-06-10 19:01:09 +0800

    优化 Result 返回

--- shrimp-cloud-common/src/main/java/com/wkclz/common/exception/BizException.java
+++ shrimp-cloud-common/src/main/java/com/wkclz/common/exception/BizException.java
@@ -1,68 +1,65 @@
 package com.wkclz.common.exception;
 
+import cn.hutool.core.util.StrUtil;
 import com.wkclz.common.emuns.ResultStatus;
 
 public class BizException extends RuntimeException {
 
     private static final String DEFAULT_ERR_MSG = "System error, please turn to Administrator";
 
+    private Integer code = -1;
+
     public BizException() {
         super();
     }
-
 
     public BizException(String msg) {
         super(msg);
     }
 
-    private Integer code = -1;
-
-
-    public static BizException remind(String msg, Object... params){
-        msg = getMsg(msg, params);
-        BizException bizException = new BizException(msg);
-        bizException.code = 0;
-        return bizException;
-    }
-    public static BizException error(String msg, Object... params){
-        msg = getMsg(msg, params);
-        BizException bizException = new BizException(msg);
-        bizException.code = -1;
-        return bizException;
+    public BizException(Integer code, String msg) {
+        super(msg);
+        this.code = code;
     }
 
-    public static BizException error(ResultStatus status){
+
+    public static BizException remind(String msg, Object... params) {
+        msg = getMsg(msg, params);
+        return new BizException(0, msg);
+    }
+
+    public static BizException error(String msg, Object... params) {
+        msg = getMsg(msg, params);
+        return new BizException(-1, msg);
+    }
+
+    public static BizException error(ResultStatus status) {
         String msg = status.getMsg();
-        BizException bizException = new BizException(msg);
-        bizException.code = status.getCode();
-        return bizException;
+        return new BizException(status.getCode(), msg);
     }
 
 
     /**
      * 标准错误返回
      */
-    public static BizException result(ResultStatus status, String msg, Object... params){
+
+    public static BizException result(Integer code, String msg, Object... params) {
         msg = getMsg(msg, params);
-        BizException bizException = new BizException(status.getMsg());
-        bizException.code = status.getCode();
-        return bizException;
+        return new BizException(code, msg);
     }
 
-    public static BizException result(Integer code, String msg){
-        BizException bizException = new BizException(msg);
-        bizException.code = code;
-        return bizException;
+    public static BizException result(ResultStatus status, String msg, Object... params) {
+        msg = getMsg(msg, params);
+        return new BizException(status.getCode(), msg);
     }
 
-
-    private static String getMsg(String msg, Object... params){
+    private static String getMsg(String msg, Object... params) {
         boolean isNull = msg == null || "".equals(msg.trim());
-        msg = isNull ?DEFAULT_ERR_MSG : msg;
-        if (params == null || params.length == 0){
+        msg = isNull ? DEFAULT_ERR_MSG : msg;
+        if (params == null || params.length == 0) {
             return msg;
         }
-        return String.format(msg, params);
+        return StrUtil.format(msg, params);
     }
 
 
