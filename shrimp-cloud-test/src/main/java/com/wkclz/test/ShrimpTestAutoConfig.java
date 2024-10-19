f6022520cf602d47493684257dbf26dd55ecfddc
wangkc <wkclz@qq.com>
2024-10-19 16:59:39 +0800

    敏感词过滤示例

---
+++ shrimp-cloud-test/src/main/java/com/wkclz/test/ShrimpTestAutoConfig.java
@@ -0,0 +1,9 @@
+package com.wkclz.test;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = {"com.wkclz.test"})
+public class ShrimpTestAutoConfig {
+}
