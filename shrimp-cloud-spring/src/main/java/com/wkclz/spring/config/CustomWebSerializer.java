237c3e6920a3fff1f1fd847c5463a67f4abc49b3
wangkc <wkclz@qq.com>
2025-01-11 00:49:06 +0800

    增加 JSON 序列化不换行设置

--- shrimp-cloud-spring/src/main/java/com/wkclz/spring/config/CustomWebSerializer.java
+++ shrimp-cloud-spring/src/main/java/com/wkclz/spring/config/CustomWebSerializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -17,6 +18,7 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * 解决 LocalDateTime 时间格式化的问题
+ * @author shrimp
  */
 @Configuration
 public class CustomWebSerializer {
@@ -32,9 +34,13 @@ public class CustomWebSerializer {
         String df = StringUtils.isBlank(dateFormat) ? DEFAULT_TIME_FORMAT : dateFormat;
         return builder -> {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern(df);
-            builder.serializerByType(LocalDateTime.class, new com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer(formatter));
+            // LocalDateTime 时间处理
+            builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(formatter));
             builder.deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(formatter));
+            // 字符串移除两端空格
             builder.deserializerByType(String.class, new TrimStringDeserializer());
+            // JSON 格式化不缩进不换行
+            builder.indentOutput(false);
         };
     }
 
